8b067fed79a6cc6c75e2e7ef84381497af07bc38
rafay186 <rafaysybrid@gmail.com>
2026-05-22 06:19:37 +0000

    variale

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,10 @@
+// const accountId = '1234322';
+const accountId = "1234321"
+var name = "hehe"
+let accountEmail = "rafay@gmail.com"
+accountCity = "Lahore"
+name = "hehe"
+accountEmail = "rafay@gmail.com"
+accountCity = "Lahore"
+console.log(accountId);
+console.table([accountId,name,accountEmail,accountCity])
