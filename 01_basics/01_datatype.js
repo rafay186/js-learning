1ac7467d3834477efcd919ecb4e5b1e76a7f1e44
rafay186 <rafaysybrid@gmail.com>
2026-05-22 07:01:38 +0000

    datatype

---
+++ 01_basics/01_datatype.js
@@ -0,0 +1,20 @@
+// treat all js as a new version
+// "use strick";
+
+//Error occur bcz of terminator
+// console.log(3+3) console.log('Rafay')
+
+// console.log(3+3); console.log('Rafay')
+
+console.log(3+3)
+
+let name= 'Rafay'
+let age= 21
+let isLoggedIn =true;
+let state;
+
+console.log(typeof name);
+console.log(typeof state);
+console.log(typeof age);
+console.log(typeof isLoggedIn);
+console.log(typeof null);
