7f26268af0ff32d1e94e52cf6293417f9a8d31ac
rafay186 <rafaysybrid@gmail.com>
2026-07-01 10:04:02 +0000

    02_practice

---
+++ 01_basics/02_variables.js
@@ -0,0 +1,15 @@
+// console.log("Rafay")
+// console.log('HEHE')
+
+const accountId = 25000
+let Name = "Rafay"
+let Email = "xyz@gmail.com"
+accountCity = "Lahore"
+accountPassword ="12344"
+
+accountCity = "Karachi"
+
+// console.log(accountId)
+// console.log(accountPassword)
+// console.log(accountCity)
+console.table([ accountId, accountPassword, Name, accountCity])
