98322df843fa9e112ed297180095197760eb130a
rafay186 <rafaysybrid@gmail.com>
2026-07-03 08:02:09 +0000

    test_done

---
+++ 01_basics/test02.js
@@ -0,0 +1,34 @@
+// Chapter_01/Q_01
+let a = "Rafay";
+let b = 45
+console.log(a + b)
+
+// Chapter_01/Q_02
+console.log(typeof (a+b))
+
+// Chapter_01/Q_03
+ 
+const a1 = {
+    Name : "Rafay",
+    Section : 1,
+    City : "Lahore",
+    Subject : "JavaScript"
+}
+// console.log(a1)
+// console.log(a1.City)
+
+// Chapter_01/Q_04
+
+a1.Friend = "ALi"
+console.log(a1)
+
+// Chapter_01/Q_04
+
+const dict={
+    veracity: "truthfulness",
+    superyacht: "luxury",
+    eccentricity: "queerness",
+    factoid: "trivialfact",
+    pancetta: "tightlyrolled"
+}
+console.log(dict)
