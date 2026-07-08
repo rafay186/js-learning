d991cfeb5347d47a0ce5594a7db140a3aaa498cf
rafay186 <rafaysybrid@gmail.com>
2026-07-08 11:04:53 +0000

    loop

---
+++ 01_basics/do_while.js
@@ -0,0 +1,13 @@
+// let n = Number.parseInt(prompt("Enter any number"));
+// i = 0;
+// while(i<n){
+//     console.log(i);
+//     i++;
+// }
+let n = Number.parseInt(prompt("Enter any number"));
+i = 10;
+do {
+  console.log(i);  
+  i++
+}
+while(i<n)
