c1000fbb3420e8730d9c1be2e207b5e71ac7f796
rafay186 <rafaysybrid@gmail.com>
2026-07-08 10:34:17 +0000

    go

--- 01_basics/sum_btw_number.js
+++ 01_basics/sum_btw_number.js
@@ -5,13 +5,32 @@ n1= Number.parseInt(n1)
 n2 = prompt("Enter Second Number")
 n2= Number.parseInt(n2)
 if(n1 > n2){
-for(i= n1; i>= n2; i ++){
+for(i= n1; i>= n2; i --){
     sum += i;
 }
 }
 else{
-    for(i= n1; i<= n2; i --){
+    for(i= n1; i<= n2; i ++){
     sum += i;
 }
 }
 console.log("The sum between the ",n1, "and", n2,"is",sum)
+
+console.log("-----------------------------")
+
+let add = 0;
+let fn = Number.parseInt(prompt("Enter First number"))
+let sn = Number.parseInt(prompt("Enter Second number"))
+let start,end;
+if(fn > sn){
+    start = fn ;
+    end = sn;
+}
+else{
+    start = sn;
+    end = fn;
+}
+for(i = start; i >= end; i--){
+    add += i
+}
+console.log("Sum from", fn, "to", sn, "is",add)
