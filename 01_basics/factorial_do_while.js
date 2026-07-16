d56b2e6d7825c56f4d5062a268196e72818ece81
rafay186 <rafaysybrid@gmail.com>
2026-07-16 11:02:51 +0000

    factorial

--- 01_basics/factorial_do_while.js
+++ 01_basics/factorial_do_while.js
@@ -1,31 +1,34 @@
-// let n = Number.parseInt(prompt("Enter any number"));
-// let factorial = 1;
-// let i = 1 ;
-// if (n === 0){
-//     console.log("Factorial of 0 is 1")
-// }
-// else{
-//     do {
-//        factorial *= i
-//         i++
-//     }
-//     while(i <= n)
+
+//        ***   Code by do - While loop   ***
+
+ let n = Number.parseInt(prompt("Enter any number"));
+let factorial = 1;
+let i = 1 ;
+if (n === 0){
+    console.log("Factorial of 0 is 1")
+}
+else{
+    do {
+       factorial *= i
+        i++
+    }
+    while(i <= n)
     
-//         console.log(" Factorial of", n , "is", factorial)
-//     }
+        console.log(" Factorial of", n , "is", factorial)
+    }
     // _____________________________
 //        ***   Code by While loop   ***
 
-let factorial = 1;
-let i = 1
-let n = Number.parseInt(prompt("Enter any number for factorial"))
-if(n == 0 ){
-    console.log("factorial of 0 is 1")
-}
-else{
-    while(i <= n){
-        factorial *= i
-        i++
-    }
-    console.log("Factorial of", n, "is", factorial)
-}
+// let factorial = 1;
+// let i = 1
+// let n = Number.parseInt(prompt("Enter any number for factorial"))
+// if(n == 0 ){
+//     console.log("factorial of 0 is 1")
+// }
+// else{
+//     while(i <= n){
+//         factorial *= i
+//         i++
+//     }
+//     console.log("Factorial of", n, "is", factorial)
+// }
