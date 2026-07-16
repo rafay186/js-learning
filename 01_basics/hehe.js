4b1e19a134e9d38e0904ec088fae3729a616d5f8
rafay186 <rafaysybrid@gmail.com>
2026-07-16 11:30:35 +0000

    hehe

---
+++ 01_basics/hehe.js
@@ -0,0 +1,12 @@
+let n = Number.parseInt( prompt("Enter even number (Agar Tu Chutia Ha tu) "));
+if (n % 2 != 0 ){
+    do{
+      n = Number.parseInt(prompt("Oyeee tu chutia haa is lie even number dal"))
+      console.log("Pata thaaa k tu chutia haaa")
+    }
+    while(n % 2 != 0);
+    }
+else{
+    console.log(" Hehe...!!! mujhe pata thaa k tu chutia haaa")
+    
+}
